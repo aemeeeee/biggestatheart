f9b49a25903dba7e703e1f5006fc9ffb7c743ee4
Man Juncheng <111041948+spinoandraptos@users.noreply.github.com>
2024-02-05 21:55:13 +0800

    Add files via upload

---
+++ BAH_Flutter/lib/Helpers/auth_service.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  Future<String> registration({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return 'Success';
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        return 'The password provided is too weak.';
+      } else if (e.code == 'email-already-in-use') {
+        return 'The account already exists for that email.';
+      } else {
+        return e.message!;
+      }
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  Future<UserCredential?> login({
+    required String email,
+    required String password,
+  }) async {
+    return await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+}
